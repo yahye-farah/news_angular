5932f55ed396384d42c49844008381d7fe6fd4fc
Yahyarashid <yahyomar63@gmail.com>
2019-02-21 01:56:37 +0200

    (feat) database helper function and some server func)

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const key = require('../key')
+const connection = mysql.createConnection({
   host: "db4free.net",
   user: "webscrapper2",
   password: "RootRoot",
@@ -13,16 +14,141 @@ connection.connect(err => {
   }
 });
 
-var save = (data) => {
+const DB = {
+  //save new user to database;
+  save: (data, callback) => {
     let query = `insert into users (username , password) values('${data.username}','${data.password}')`;
 
-    connection.query(query , (err,results) => {
-        if(err) {
-            console.log(err)
-        }else {
-            console.log('saved to database')
-        }
-    })
-}
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('results', results)
+        // let token = jwt.sign(data.username,key.secret, { expiresIn: '1h' });
+        let token = jwt.sign({ username: data.username },
+          key.secret,
+          {
+            expiresIn: '24h' // expires in 24 hours
+          }
+        );
+        callback(token)
 
-module.exports.save = save;
+      }
+    })
+  },
+
+  //check if the new user is already exist 
+  userExists: (data, callback) => {
+    let query = `select * from users where username='${data.username}'`
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('is user exist in the database', results);
+        callback(results);
+      }
+    })
+  },
+
+  saveNews: (news, callback) => {
+
+    for (let data of news) {
+      let query = `insert into news (newstext, author, title, description, url) values("${data.content}","${data.author}","${data.title}",
+    "${data.description}","${data.url}")`;
+      connection.query(query, (err, results) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('saved to database');
+        }
+      })
+    }
+
+  },
+
+  //get all news in the database and users and comments
+  allNews: (callback) => {
+    let allnews = [];
+    let query = `select * from news`
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('yyyyy', results)
+        for (let result of results) {
+          let obj = {};
+          obj['news'] = result;
+
+          let query = `select * from comments where news_id='${result.id}'`
+          connection.query(query, (err, comments) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('comments', comments)
+              obj['comments'] = comments;
+              result['comments'] = comments;
+              if (comments.length > 0) {
+                // console.log('shurrud',comments[0]['user_id'])
+                let query = `select username from users where id ='${comments[0].user_id}'`
+                connection.query(query, (err, user) => {
+                  if (err) {
+                    console.log(err)
+                  } else {
+                    // console.log('username',user[0]['username'])
+                    result['username'] = user[0]['username'];
+                    obj['username'] = user[0]['username'];
+                    console.log('obbbb', obj)
+                    allnews.push(obj);
+                    callback(allnews)
+                  }
+                })
+              }
+            }
+          })
+
+        }
+      }
+      
+    })
+  },
+
+  //save comments to database
+  comment: (data, callback) => {
+    let query = `insert into comments (comment, user_id, news_id) values ('${data.comment}', '${data.user_id}', '${data.news_id}')`
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("comment saved to database")
+        callback('news is commented');
+      }
+    })
+  },
+
+  //update comments 
+
+  editComment: (data, callback) => {
+    let query = `update comments set comment= '${data.comment}' where id = '${data.id}'`;
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('the comment is updated successfuly');
+      }
+    })
+  },
+
+  //delete comment 
+  deleteComment: (data, callback) => {
+    let query = `delete from comments where id='${data.id}'`;
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('successfuly deleted from the database');
+      }
+    })
+  }
+
+}
+module.exports = DB;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const DB = require('./db/index');
 const cors = require('cors');
 const request = require('request')
-const key = require('./key')
+const key = require('./key');
+const news = require('./data')
 
 
 const app = express();
@@ -11,15 +13,85 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-request.get(`{key}`, (error, response, body) => {
-    console.log('news',response)
+//call the api to get the news 
+
+// request.get(key.apiKey, (error, response, body) => {
+//     console.log('jjjj',body)
+//     body = JSON.parse(body);
+//     console.log('news',body.articles)
+//     DB.saveNews(body.articles);
+// })
+
+
+//get all the news and their comments and users that made the comment
+
+app.get('/',(req,res) => {
+    DB.allNews((result) => {
+        res.send(result)
+    })
 })
 
+
+
+
+//register new user 
 app.post('/register', (req , res) => {
-    console.log('you reached me')
-    //DB.save(req.body.)
+    console.log('you reached me',req.body.data)
+    DB.userExists(req.body.data , (user) => {
+        if(user.length > 0) {
+            res.send('This user is already exist please choose another username');
+        }else {
+            DB.save(req.body.data, ( token) => {
+                res.send(token);
+            })
+        }
+    })
 })
 
+// app.post('/login', (req,res) => {
+
+// })
+
+//authorization middle-ware
+const checkToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+
+        req.token = token;
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403)
+    }
+}
+
+
+app.post('/comment', (req, res) => {
+    DB.comment(req.body.data, (result) => {
+        res.send(result);
+    });
+});
+
+
+
+app.post('/updatecomment',checkToken,(req,res) => {
+   
+    jwt.verify(req.token, key.secret, (err, authorizedData) => {
+        if(err){
+            //If error send Forbidden (403)
+            console.log('ERROR: Could not connect to the protected route');
+            res.sendStatus(403);
+        } else {
+            //If token is successfully verified, we can send the autorized data 
+            console.log('sensative data',authorizedData);
+            console.log('SUCCESS: Connected to protected route');
+        }
+    })
+
+})
 
 app.listen(3000, function(){
     console.log('server is running 3000')
